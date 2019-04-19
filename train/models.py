81c8349e5ec116ea4131b2f40db55ac39703a840
Dipesh <dipeshsukhani@gmail.com>
2019-04-19 19:08:32 +0800

    Train Model and Migration
    Created a Model of the Train; migrated it and then added a dummy train to the models using the shell: python manage.py shell

--- train/models.py
+++ train/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Train(models.Model):
+    origin = models.CharField(max_length=64)
+    destination = models.CharField(max_length=64)
+    duration = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id}: {self.origin} to {self.destination} in {self.duration} mins"
