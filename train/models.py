ddf39b211d15a7e7b3b2420e044bf835c0664002
Dipesh <dipeshsukhani@gmail.com>
2019-04-20 14:14:56 +0800

    Created Models Train and Station and dummy data

--- train/models.py
+++ train/models.py
@@ -3,9 +3,19 @@ from django.db import models
 # Create your models here.
 
 
+class Station(models.Model):
+    city = models.CharField(max_length=64)
+    code = models.CharField(max_length=3)
+
+    def __str__(self):
+        return f"{self.city} ({self.code})"
+
+
 class Train(models.Model):
-    origin = models.CharField(max_length=64)
-    destination = models.CharField(max_length=64)
+    origin = models.ForeignKey(
+        Station, on_delete=models.CASCADE, related_name="departures")
+    destination = models.ForeignKey(
+        Station, on_delete=models.CASCADE, related_name="arrival")
     duration = models.IntegerField()
 
     def __str__(self):
